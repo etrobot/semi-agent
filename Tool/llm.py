5b2dbb1e05194e67b92fe3003b65d91c14625220
franklin <etrobot@outlook.com>
2023-11-25 22:12:12 +0800

    add wechat post rip func

--- Tool/llm.py
+++ Tool/llm.py
@@ -1,3 +1,8 @@
+import re
+
+import requests
+from bs4 import BeautifulSoup
+
 from config import MODEL,KEYS
 from litellm import completion
 def summarize(text:str):
@@ -8,7 +13,7 @@ def summarize(text:str):
     }], api_key=KEYS[MODEL])["choices"][0]["message"]["content"]
 
 def makelist(prompt:str,subList=False):
-    suffix =' Output keypoints with index'
+    suffix =' Output points with index'
     if subList:
         suffix=' and output "\nkeyword1 keyword2 keyword3\nkeyword4 keyword5 keyword6\n...",do not output any punctuation or symbol'
     msg = [{
@@ -20,3 +25,33 @@ def makelist(prompt:str,subList=False):
     reply_text = response["choices"][0]["message"]["content"]
     print(reply_text.split('\n'))
     return [x for x in reply_text.split('\n') if len(x)>2]
+
+def wechatPost(url:str):
+    res = requests.get(url)
+    soup = BeautifulSoup(res.text, "html.parser")
+    query = soup.get_text(separator="\n")
+    for s in soup(['script', 'style']):
+        s.decompose()
+    soup=soup.find(id='js_content')
+    query1 = [x.get_text(separator='\n') for x in soup.find_all('section')]
+    query2 = [x.get_text(separator='\n') for x in soup.find_all('p')]
+    if len(''.join(query2)) > len(''.join(query1)):
+        query1 = query2
+    if len('\n'.join(query1)) == 0:
+        queryText = re.sub(r'\\x[0-9a-fA-F]{2}', '',
+                           soup.find('meta', {'name': 'description'}).attrs['content'])
+    else:
+        query1 = '\n'.join(query1).split('\n')
+        query = list(set(query1))
+        query.sort(key=query1.index)
+        queryText = '\n'.join(query)
+    print(query[0],len(queryText))
+    return queryText
+def ask(prompt:str):
+    return completion(model=MODEL, messages=[{
+        "role": "user",
+        "content": prompt,
+    }], api_key=KEYS[MODEL])["choices"][0]["message"]["content"]
+
+if __name__=='__main__':
+    print(wechatPost('https://mp.weixin.qq.com/s/fVTxYWc4f2x9W710mMQzJA'))
